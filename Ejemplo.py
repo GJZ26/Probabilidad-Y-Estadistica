f0e5e2d3f5ef4389be5ec60e552611a87d2901fa
GJZ26 <adolfogj26@gmail.com>
2023-04-30 22:12:46 -0600

    First commit

---
+++ Ejemplo.py
@@ -0,0 +1,82 @@
+import PyE_tools as pye
+
+# Este es un script de cómo usar el módulo de PyE_tools...
+
+# Los datos admitidos son de tipo lista, si lo almacenan Vectores, favor de convertirlo
+datos = [782,1333,515,1475,696,832,1052,700,987,542,1296,704,
+         814,1482,1023,739,643,956,1023,784]
+
+# Aquí hay otros datos de ejemplos para que puedas probar el algoritmo,
+# recuerda volver a comentar o eliminar la lista anterior, de tal modo que solo
+# tengas una lista activa durante la ejecución.
+
+# datos = [1.82,1.43,1.51,1.47,1.69,1.88,1.52,1.72,1.78,1.54,1.61,1.66,
+#          1.70,1.81,1.58,1.48,1.53,1.73,1.61,1.56,1.57,1.78]
+
+# datos = [87,66,73,68,48,37,76,85,74,65,93,77,66,83,78,49,57,38,69,78,89,
+#          96,78,97,74,76,68,63,70,81,64,83,67,61,90,77,88,74,85,80,71,73,
+#          61,57,72,80,77,85,80,89]
+
+# datos = [128,146,136,136,150,140,124,134,142,138,136,120,130,136,132,136,
+#          134,142,132,144]
+
+# Es importante tratar la lista antes de empezar a usar la librería
+# Usa la función listToDecimal para tratar las variables numéricas de las
+# listas y evitar errores de redondeo por decimales...
+pye.listToDecimal(datos)
+
+# Puedes ordenar los datos de forma manual, o con las funciones
+# de ordenamiento del módulo, estos métodos son:
+#   * bubbleSort (Recomendado)
+#   * gnomeSort
+#   * insertionSort
+#   * selectionSort
+# 
+# Estas funciones NO modifican la lista original, por lo que es necesario
+# guardar la lista ordenada en una nueva variable.
+datosOrdenados = pye.bubbleSort(datos)
+
+# klassesNumber cuenta las clases que tendrá la tabla de distribución de frecuencia
+# No es necesario tener los datos ordenados, retorna un número entero.
+k = pye.klassesNumber(datos)
+
+# dataRange calcula el rango de datos de datos de una lista, recibe dos parámetros
+# list: La lista de datos
+# isSorted: Un boleano para saber si la lista está ordenada, por defecto es True.
+# Retorna un Int o Float
+r = pye.dataRange(datosOrdenados)
+
+# dataAmplitudeByList es una función que calcula la amplitud de las clases de la
+# tabla de frecuencia, al igual que la función anterior, recibe dos parámetros
+# list: La lista de datos
+# isSorted: Un boleano para saber si la lista está ordenada, por defecto es True.
+# Retorna un Decimal
+# Puedes usar
+a = pye.dataAmplitudeByList(datosOrdenados,True)
+
+# Esta función identifica la unidad de variación de los datos, solo recibe un parámetro
+#   * data: Lista con los datos, puede o no estar tratada u ordenada.
+# Retorna un Decimal
+uv = pye.variationUnit(datosOrdenados)
+
+# Genera una lista bidimensional con los datos necesarios para generar una tabla,
+# Recibe 5 parámetros:
+#   * sortedData: lista con los datos ordenados,
+#   * classesNumber: entero con el número de clases para la tabla,
+#   * dataRange: Rango de los datos por clase de la tabla,
+#   * amplitude: Amplitud de la tabla de datos,
+#   * variationUnit: Unidad de variación de los datos,
+# Retorna un lista bidiomensional
+# Lista primer nivel: [[...],[...],[...],...]
+# Lista segundo nivel: [LetraDeLaClase, LímiteInferior, LimiteSuperior, Frecuencia, Marca de la Clase, Limite Inferior Exacto, Limite Superior Exacto]
+dataTable = pye.calculateFrecuencyByDataList(datosOrdenados,k,r,a,uv)
+
+
+print(f'Número de Clases: {k}')
+print(f'Rango: {r}')
+print(f'Amplitud: {a}')
+print(f'Unidad de Variación: {uv}')
+
+# drawTable imprime la información formateada del retorno de la función calculateFrecuencyByDataList
+# NO usar con ningún otro tipo de lista    
+pye.drawTable(dataTable)
---
+++ PyE_tools.py
@@ -0,0 +1,184 @@
+import math
+from decimal import Decimal
+
+# Versión pre-alpha :), se puede usar, pero es suuuuper inestable...
+
+# Créditos por iluminarme durante la elaboración de esta vaina:
+#   * https://github.com/Leonardo-Toledo-V
+#   * https://github.com/ShonSagoro
+
+
+
+# Funciones de ordamiento de datos (Sólo numéricos) - Estables
+# Crédito: https://github.com/LWH-21
+
+def bubbleSort(data: list):
+    result = data.copy()
+    permutation = True
+    iteration = 0
+    while permutation == True:
+        permutation = False
+        iteration = iteration + 1
+        for actual in range(0, len(result) - iteration):
+            if result[actual] > result[actual + 1]:
+                permutation = True
+                result[actual], result[actual + 1] = result[actual + 1], result[actual]
+    return result
+
+
+def gnomeSort(data: list):
+    result = data.copy()
+    i_b, i_i, dataLength = 1, 2, len(result)
+    while i_b < dataLength:
+        if result[i_b - 1] <= result[i_b]:
+            i_b, i_i = i_i, i_i + 1
+        else:
+            result[i_b - 1], result[i_b] = result[i_b], result[i_b - 1]
+            i_b -= 1
+            if i_b == 0:
+                i_b, i_i = i_i, i_i + 1
+    return result
+
+
+def insertionSort(data: list):
+    result = data.copy()
+    for i in range(1, len(result)):
+        actual = result[i]
+        j = i
+        while j > 0 and result[j - 1] > actual:
+            result[j] = result[j - 1]
+            j = j - 1
+        result[j] = actual
+    return result
+
+
+# Funciones de ordenamiento (Sólo numérico) - Inestable
+# Crédito: https://github.com/LWH-21
+
+
+def selectionSort(data: list):
+    result = data.copy()
+    nb = len(result)
+    for actual in range(0, nb):
+        mas_pequeno = actual
+        for j in range(actual + 1, nb):
+            if result[j] < result[mas_pequeno]:
+                mas_pequeno = j
+        if min is not actual:
+            temp = result[actual]
+            result[actual] = result[mas_pequeno]
+            result[mas_pequeno] = temp
+    return result
+
+
+# Funciones de normalización de tipo de variables
+
+
+def listToDecimal(data: list):
+    for i in range(0, len(data)):
+        data[i] = Decimal(str(data[i]))
+    return data
+
+
+# Funciones para cálculo de Distribuciones de Frecuencias :)
+
+letterForNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def klassesNumber(data: list):
+    return round(1 + 3.322 * math.log10(len(data)))
+
+
+def dataRange(sortedData: list, isSorted: bool = True):
+    if isSorted:
+        return sortedData[len(sortedData) - 1] - sortedData[0]
+    else:
+        tempList = bubbleSort(sortedData)
+        return tempList[len(tempList) - 1] - tempList[0]
+
+
+def dataAmplitudeByList(data: list, isSorted: bool = True):
+    range = (dataRange(data, isSorted))
+    classesNumber = (klassesNumber(data))
+    if(variationUnit(data) == 1):
+        return Decimal(str(math.ceil(Decimal(str(range/classesNumber)))))
+    
+    return Decimal(str((math.ceil(Decimal(str(range / classesNumber))*10))/10))
+
+
+def dataAmplitudeByVariables(range: int, classesNumber: int):
+    return math.ceil(range / classesNumber)
+
+
+def variationUnit(data: list):
+    maxDecimalNumber = 0
+    for i in data:
+        if "." in str(i):
+            maxDecimalNumber = (
+                len(str(i).split(".")[1])
+                if len(str(i).split(".")[1]) > maxDecimalNumber
+                else maxDecimalNumber
+            )
+
+    return Decimal(str(math.pow(10, -(maxDecimalNumber))))
+
+
+def calculateFrecuencyByDataList(
+    sortedData: list,
+    classesNumber: int,
+    dataRange: Decimal,
+    amplitude: Decimal,
+    variationUnit: Decimal,
+):
+    result = []
+    limInf = sortedData[0]
+
+    for i in range(0, classesNumber):
+        limSup = limInf + amplitude - variationUnit
+        result.append(
+            [
+                letterForNames[i],  # Clase
+                limInf,  # Límite Inferior
+                limSup,  # Límite Superior
+                frecuencyInRange(sortedData, limInf, limSup),  # Frecuencia
+                (limSup + limInf) / 2,  # Marca de la clase
+                limInf - (variationUnit / 2),  # Limite Inferior Exacto
+                limSup + (variationUnit / 2),  # Límite Superior Exacto
+            ]
+        )
+        limInf = limSup + variationUnit
+
+    return result
+
+
+def frecuencyInRange(data: list, limInf: Decimal, limSup: Decimal):
+    frecuency = 0
+    for i in data:
+        if i <= limSup and i >= limInf:
+            frecuency += 1
+    return frecuency
+
+
+def drawTable(data: list):
+    print(
+        "{:<8}{:<15}{:<15}{:<10}{:<20}{:<18}{:<10}".format(
+            "Clase",
+            "Límite Inf.",
+            "Límite Sup.",
+            "Frec.",
+            "Marca d Clases",
+            "Lim Inf Exac",
+            "Lim Sup Exac",
+        )
+    )
+    print(
+        "{:<8}{:<15}{:<15}{:<10}{:<20}{:<18}{:<10}".format(
+            "---", "---", "---", "---", "---", "---", "---"
+        )
+    )
+    for i in data:
+        print(
+            "{:<8}{:<15}{:<15}{:<10}{:<20}{:<18}{:<10}".format(
+                i[0], i[1], i[2], i[3], i[4], i[5], i[6]
+            )
+        )
